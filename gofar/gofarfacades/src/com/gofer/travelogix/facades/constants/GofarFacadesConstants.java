e03609d7d0da0fde56c3b8a5b4bff2f11bb46a98
Himanshi <himanshi@pragiti.com>
2017-01-17 13:17:47 +0530

    Extensions

---
+++ gofar/gofarfacades/src/com/gofer/travelogix/facades/constants/GofarFacadesConstants.java
@@ -0,0 +1,26 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.gofer.travelogix.facades.constants;
+
+/**
+ * Global class for all GofarFacades constants.
+ */
+@SuppressWarnings("PMD")
+public class GofarFacadesConstants extends GeneratedGofarFacadesConstants
+{
+	public static final String EXTENSIONNAME = "gofarfacades";
+
+	private GofarFacadesConstants()
+	{
+		//empty
+	}
+}
---
+++ gofar/gofarfulfilmentprocess/src/com/gofer/travelogix/fulfilmentprocess/jalo/GofarFulfilmentProcessManager.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.gofer.travelogix.fulfilmentprocess.jalo;
+
+import de.hybris.platform.jalo.JaloSession;
+import de.hybris.platform.jalo.extension.ExtensionManager;
+import com.gofer.travelogix.fulfilmentprocess.constants.GofarFulfilmentProcessConstants;
+
+@SuppressWarnings("PMD")
+public class GofarFulfilmentProcessManager extends GeneratedGofarFulfilmentProcessManager
+{
+	public static final GofarFulfilmentProcessManager getInstance()
+	{
+		ExtensionManager em = JaloSession.getCurrentSession().getExtensionManager();
+		return (GofarFulfilmentProcessManager) em.getExtension(GofarFulfilmentProcessConstants.EXTENSIONNAME);
+	}
+	
+}
