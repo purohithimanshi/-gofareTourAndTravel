51ad596a9e5353302f11763717280f3e4f5e2a26
Himanshi <rajhimanshi28@gmail.com>
2017-02-17 13:28:18 +0530

    System set up class for initial data.

--- gofar/gofarinitialdata/src/com/gofar/tourtravel/initialdata/setup/InitialDataSystemSetup.java
+++ gofar/gofarinitialdata/src/com/gofar/tourtravel/initialdata/setup/InitialDataSystemSetup.java
@@ -14,31 +14,35 @@ package com.gofar.tourtravel.initialdata.setup;
 import de.hybris.platform.commerceservices.dataimport.impl.CoreDataImportService;
 import de.hybris.platform.commerceservices.dataimport.impl.SampleDataImportService;
 import de.hybris.platform.commerceservices.setup.AbstractSystemSetup;
+import de.hybris.platform.commerceservices.setup.data.ImportData;
+import de.hybris.platform.commerceservices.setup.events.CoreDataImportedEvent;
+import de.hybris.platform.commerceservices.setup.events.SampleDataImportedEvent;
 import de.hybris.platform.core.initialization.SystemSetup;
 import de.hybris.platform.core.initialization.SystemSetup.Process;
 import de.hybris.platform.core.initialization.SystemSetup.Type;
 import de.hybris.platform.core.initialization.SystemSetupContext;
 import de.hybris.platform.core.initialization.SystemSetupParameter;
 import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
-import com.gofar.tourtravel.initialdata.constants.GofarInitialDataConstants;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Required;
+
+import com.gofar.tourtravel.initialdata.constants.GofarInitialDataConstants;
 
 
 /**
  * This class provides hooks into the system's initialization and update processes.
- * 
+ *
  * @see "https://wiki.hybris.com/display/release4/Hooks+for+Initialization+and+Update+Process"
  */
 @SystemSetup(extension = GofarInitialDataConstants.EXTENSIONNAME)
 public class InitialDataSystemSetup extends AbstractSystemSetup
 {
 	@SuppressWarnings("unused")
-	private static final Logger LOG = Logger.getLogger(InitialDataSystemSetup.class);
+	private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(InitialDataSystemSetup.class);
 
 	private static final String IMPORT_CORE_DATA = "importCoreData";
 	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
@@ -46,6 +50,8 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 
 	private CoreDataImportService coreDataImportService;
 	private SampleDataImportService sampleDataImportService;
+
+	private static final String GOFAR = "gofar";
 
 	/**
 	 * Generates the Dropdown and Multi-select boxes for the project data import
@@ -67,13 +73,15 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	/**
 	 * Implement this method to create initial objects. This method will be called by system creator during
 	 * initialization and system update. Be sure that this method can be called repeatedly.
-	 * 
+	 *
 	 * @param context
 	 *           the context provides the selected parameters and values
 	 */
 	@SystemSetup(type = Type.ESSENTIAL, process = Process.ALL)
 	public void createEssentialData(final SystemSetupContext context)
 	{
+		importImpexFile(context, "/gofarinitialdata/import/coredata/common/essential-data.impex");
+		LOG.debug("InitialDataSystemSetup : createEssentialData() : ");
 		// Add Essential Data here as you require
 	}
 
@@ -104,9 +112,70 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	@SystemSetup(type = Type.PROJECT, process = Process.ALL)
 	public void createProjectData(final SystemSetupContext context)
 	{
-		/*
-		 * Add import data for each site you have configured
-		 */
+		LOG.debug("InitialDataSystemSetup : createProjectData() : ");
+		// Importing gofar site data
+
+		setUpSitedata(GOFAR, context);
+
+		LOG.debug("InitialDataSystemSetup : createProjectData() : Exiting.");
+	}
+
+	/**
+	 * @param pSiteName
+	 *           is the site name to be considered
+	 * @param pContext
+	 *           is the selected parameters
+	 */
+	private void setUpSitedata(final String pSiteName, final SystemSetupContext pContext)
+	{
+		final List<ImportData> importDatas = new ArrayList<ImportData>();
+		final ImportData gofarImportData = new ImportData();
+		gofarImportData.setStoreNames(Arrays.asList(pSiteName));
+		gofarImportData.setContentCatalogNames(Arrays.asList(pSiteName));
+		gofarImportData.setProductCatalogName(pSiteName);
+		importDatas.add(gofarImportData);
+		importSyncData(pSiteName, pContext);
+
+
+		getCoreDataImportService().execute(this, pContext, importDatas);
+		getEventService().publishEvent(new CoreDataImportedEvent(pContext, importDatas));
+		getSampleDataImportService().execute(this, pContext, importDatas);
+		getEventService().publishEvent(new SampleDataImportedEvent(pContext, importDatas));
+
+	}
+
+	/**
+	 * @param pSiteName
+	 * @param pContext
+	 */
+	private void importSyncData(final String pSiteName, final SystemSetupContext pContext)
+	{
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/coredata/contentCatalogs/catalogName/catalog.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/coredata/contentCatalogs/catalogName/catalog_en.impex");
+		importImpexFile(pContext, "/gofarinitialdata/resources/gofarinitialdata/import/coredata/stores/storeName/site.impex");
+		importImpexFile(pContext, "/gofarinitialdata/resources/gofarinitialdata/import/coredata/stores/storeName/site_en.impex");
+		importImpexFile(pContext, "/gofarinitialdata/resources/gofarinitialdata/import/coredata/stores/storeName/store.impex");
+		importImpexFile(pContext, "/gofarinitialdata/resources/gofarinitialdata/import/coredata/stores/storeName/store_en.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/contentCatalogs/catalogName/cms-content.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/contentCatalogs/catalogName/cms-content_en.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/contentCatalogs/catalogName/email-content.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/contentCatalogs/catalogName/email-content_en.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/productCatalogs/catalogName/categories.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/productCatalogs/catalogName/categories_en.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/productCatalogs/catalogName/products.impex");
+		importImpexFile(pContext,
+				"/gofarinitialdata/resources/gofarinitialdata/import/sampledata/productCatalogs/catalogName/products_en.impex");
+
+
 	}
 
 	public CoreDataImportService getCoreDataImportService()
